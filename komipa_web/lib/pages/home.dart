320736b98e248a695f6aea3963dd2a8582b8a367
josiahfs <josiahfrlswt@gmail.com>
2022-11-22 11:57:32 +0700

    landing page

--- komipa_web/lib/pages/home.dart
+++ komipa_web/lib/pages/home.dart
@@ -1,89 +1,131 @@
+import 'package:adaptive_navbar/adaptive_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width * 0.05,
-            color: Color(0xffC76100),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
+    final sw = MediaQuery.of(context).size.width;
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/home-bg.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AdaptiveNavBar(
+          backgroundColor: Color(0xffC76100),
+          screenWidth: sw,
+          title: const Text(
+            "KOMIPA",
+            style: TextStyle(fontSize: 40),
+          ),
+          centerTitle: false,
+          navBarItems: [
+            NavBarItem(
+              text: "Beranda",
+              onTap: () {
+                Navigator.pushNamed(context, "routeName");
+              },
+            ),
+            NavBarItem(
+              text: "Tentang Kami",
+              onTap: () {
+                Navigator.pushNamed(context, "routeName");
+              },
+            ),
+            NavBarItem(
+              text: "Menu",
+              onTap: () {
+                Navigator.pushNamed(context, "routeName");
+              },
+            ),
+            NavBarItem(
+              text: "Login",
+              onTap: () {
+                Navigator.pushNamed(context, "routeName");
+              },
+            ),
+          ],
+        ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 64.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'KOMIPA',
-                    style: TextStyle(
-                        fontSize: 40,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.bold),
+                  SizedBox(
+                    height: 220,
                   ),
-                  Spacer(),
+                  Row(
+                    children: [
+                      Text(
+                        'Selamat datang di ',
+                        style: GoogleFonts.inter(
+                            fontSize: 60,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'KOMIPA,',
+                        style: GoogleFonts.inter(
+                          fontSize: 60,
+                          color: Color(0xffF5B309),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
                   Text(
-                    'beranda',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.w300),
+                    'Kuliah rutinitas, ngopi prioritas!',
+                    style: GoogleFonts.inter(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontStyle: FontStyle.italic,
+                    ),
                   ),
                   SizedBox(
-                    width: 59,
+                    height: 20,
                   ),
-                  Text(
-                    'Tentang Kami',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    width: 59,
-                  ),
-                  Text(
-                    'Menu',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    width: 59,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(
-                            color: Color(0xffffffff),
+                  Container(
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.transparent),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.white)),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 12),
+                        child: Text(
+                          'RESERVASI',
+                          style: GoogleFonts.inter(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
                       ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xfff6f6f6),
-                            fontWeight: FontWeight.w300),
-                      ),
+                      onPressed: () {},
                     ),
                   )
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
