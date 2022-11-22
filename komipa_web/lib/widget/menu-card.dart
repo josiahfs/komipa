6ec18ae942cb1920d9af9198446d3249cf513d96
josiahfs <josiahfrlswt@gmail.com>
2022-11-23 03:00:46 +0700

    add hover state to menu card

--- komipa_web/lib/widget/menu-card.dart
+++ komipa_web/lib/widget/menu-card.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MenuCard extends StatelessWidget {
+class MenuCard extends StatefulWidget {
   String image;
   String name;
   String price;
@@ -11,67 +9,91 @@ class MenuCard extends StatelessWidget {
   MenuCard({required this.image, required this.name, required this.price});
 
   @override
+  State<MenuCard> createState() => _MenuCardState();
+}
+
+class _MenuCardState extends State<MenuCard> {
+  bool isHover = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 180,
-      height: 231,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          // color: Colors.white,
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0.1, 0.3],
-            colors: [
-              Color(0xffDA9655).withOpacity(0.1),
-              Color(0xffF6F6F6),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {},
+        onHover: (value) {
+          setState(() {
+            isHover = value;
+          });
+        },
+        child: Container(
+          width: 180,
+          height: 231,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              (isHover)
+                  ? BoxShadow(
+                      color: Color(0xffDA9655),
+                      spreadRadius: 10,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    )
+                  : BoxShadow(color: Color(0xffDA9655))
             ],
-          )),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              child: Image.asset(
-                image,
-                height: 120,
-                width: 170,
-              ),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.1, 0.3],
+              colors: [
+                Color(0xffDA9655).withOpacity(0.1),
+                Color(0xffF6F6F6),
+              ],
             ),
-            Container(
-              height: 111,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                    child: Flexible(
-                      child: Text(
-                        name,
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.inter(
-                            fontSize: 16, fontWeight: FontWeight.w600),
-                      ),
-                    ),
+          ),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Image.asset(
+                    widget.image,
+                    height: 120,
+                    width: 170,
                   ),
-                  // SizedBox(
-                  //   height: 20,
-                  // ),
-                  Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 40.0),
-                    child: Text(
-                      'Rp.$price',
-                      style: GoogleFonts.inter(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xffC76100),
+                ),
+                Container(
+                  height: 111,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: Flexible(
+                          child: Text(
+                            widget.name,
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.inter(
+                                fontSize: 16, fontWeight: FontWeight.w600),
+                          ),
+                        ),
                       ),
-                    ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 40.0),
+                        child: Text(
+                          'Rp.${widget.price}',
+                          style: GoogleFonts.inter(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xffC76100),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ]),
+                ),
+              ]),
+        ),
+      ),
     );
   }
 }
