2f03607d8868d4a4fb74b0e10bd700ecbfbe097a
josiahfs <josiahfrlswt@gmail.com>
2022-11-24 21:46:48 +0700

    biar bisa pull

--- komipa_web/lib/main.dart
+++ komipa_web/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: ConfirmPage(),
-      home: HomePage(),
+      home: PaymentPage(),
     );
   }
 }
