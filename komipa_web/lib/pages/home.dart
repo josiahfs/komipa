608024415f786e9907ab4356b036cb0abf210316
Muhammad Linggar Ryanidha <Muhammad Linggar Ryanidha>
2022-11-24 22:13:44 +0700

    fixing

--- komipa_web/lib/pages/home.dart
+++ komipa_web/lib/pages/home.dart
@@ -1,97 +1,32 @@
 import 'package:adaptive_navbar/adaptive_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'package:komipa_web/pages/LoginPage/login.dart';
-
 import 'package:komipa_web/pages/home/about.dart';
 import 'package:komipa_web/pages/home/cta.dart';
 import 'package:komipa_web/pages/home/landing.dart';
 import 'package:komipa_web/pages/home/menu.dart';
-
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //Kenapa enggak pake appBar aja?
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width * 0.05,
-            color: Color(0xffC76100),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Text(
-                    'KOMIPA',
-                    style: TextStyle(
-                        fontSize: 40,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Spacer(),
-                  Text(
-                    'beranda',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    width: 59,
-                  ),
-                  Text(
-                    'Tentang Kami',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    width: 59,
-                  ),
-                  Text(
-                    'Menu',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xfff6f6f6),
-                        fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    width: 59,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Color(0xffC76100)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                          side: BorderSide(
-                            color: Color(0xffffffff),
-                          ),
-                        ),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xfff6f6f6),
-                            fontWeight: FontWeight.w300),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
+    final sw = MediaQuery.of(context).size.width;
+    final sh = MediaQuery.of(context).size.height;
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/home-bg.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AdaptiveNavBar(
+          backgroundColor: Color(0xffC76100),
+          screenWidth: sw,
+          title: Text(
+            "KOMIPA",
+            style: GoogleFonts.inter(fontWeight: FontWeight.bold, fontSize: 24),
           ),
           centerTitle: false,
           navBarItems: [
@@ -116,8 +51,7 @@ class HomePage extends StatelessWidget {
             NavBarItem(
               text: "Login",
               onTap: () {
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => LoginPage()));
+                Navigator.pushNamed(context, "routeName");
               },
             ),
           ],
